c17c1c73a5c6bc79434ecc99ca591df55a3db57a
Finn-Delimata-May <D00276939@student.dkit.ie>
2025-01-03 16:18:57 +0000

    Compare Mean of Experiment to Confidence Interval
    The upper and lower limits of the Control's 95% Confidence Interval are
    printed to screen, and compared to the mean of the VR experiment. Since
    the VR experiment's mean (9.11), is outside the upper limit of the
    Control's confidence interval, (8.08), we reject the null hypothesis.

--- RScript1.R
+++ RScript1.R
@@ -3,10 +3,12 @@ participants_data_final_control <- read_excel("College Documents/Sem1_Data_Analy
 tics_for_Immersive_Environments/participants_data_final_control.xlsx")
 
 # The lines below establish variables from the data read in from the control 
-# group. "mc" means the mean from the control. 
-var.mc = mean(participants_data_final_control$Change_in_Score)
+# group.  
+var.mc = mean(participants_data_final_control$Change_in_Score) # "mc" means the 
+                                                               # mean from the 
+                                                               # control.
 var.s = sd(participants_data_final_control$Change_in_Score)
-var.rows = nrow(participants_data_final_control)
+var.df = nrow(participants_data_final_control) - 1
 
 library(readxl)
 participants_data_final_xp <- read_excel("College Documents/Sem1_Data_Analy
@@ -19,3 +21,5 @@ var.se = var.s/sqrt(var.rows)   # Calculates Standard Error
 var.nse = qt(0.975, var.rows)   # The number of SEs away from the mean
 print(var.mc + var.nse*var.se)
 print(var.mc - var.nse*var.se)
+
+print(var.mx)
