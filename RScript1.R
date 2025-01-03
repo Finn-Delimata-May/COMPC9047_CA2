fe49854f62fda07dadaa23deecc50c985209b46e
Finn-Delimata-May <D00276939@student.dkit.ie>
2025-01-03 15:15:35 +0000

    Read in and process data
    VR experiment data and control data, which have been separated into two
    separate tables, have been read in and initially analysed to get the
    mean, median, and standard deviation.

---
+++ RScript1.R
@@ -0,0 +1,11 @@
+library(readxl)
+participants_data_final_control <- read_excel("College Documents/Sem1_Data_Analy
+tics_for_Immersive_Environments/participants_data_final_control.xlsx")
+
+plot(participants_data_final_control$Change_in_Score)
+summary(participants_data_final_control$Change_in_Score)
+sd(participants_data_final_control$Change_in_Score)
+
+library(readxl)
+participants_data_final_xp <- read_excel("College Documents/Sem1_Data_Analy
+tics_for_Immersive_Environments/participants_data_final_xp.xlsx")
