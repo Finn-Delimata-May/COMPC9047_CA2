642074240b62e7a8de75debc14dc61f6dc877ed9
Finn-Delimata-May <D00276939@student.dkit.ie>
2025-01-03 15:40:36 +0000

    Calculate Standard Error and Save Variables
    Certain parameters, like the standard deviation, and number of rows,
    were saved as variables, to make writing code later on easier.
    
    The Standard Error was calculated using the standard deviation and
    number of samples for the control. The control was chosen as the base,
    since Cognitive Behaviour Therapy is the older and more common form, and
    the one we're comparing the VR Therapy to.

--- RScript1.R
+++ RScript1.R
@@ -4,8 +4,11 @@ tics_for_Immersive_Environments/participants_data_final_control.xlsx")
 
 plot(participants_data_final_control$Change_in_Score)
 summary(participants_data_final_control$Change_in_Score)
-sd(participants_data_final_control$Change_in_Score)
+var.s = sd(participants_data_final_control$Change_in_Score)
+var.rows = nrow(participants_data_final_control$Change_in_Score)
 
 library(readxl)
 participants_data_final_xp <- read_excel("College Documents/Sem1_Data_Analy
 tics_for_Immersive_Environments/participants_data_final_xp.xlsx")
+
+var.se = var.s/sqrt(var.rows)
