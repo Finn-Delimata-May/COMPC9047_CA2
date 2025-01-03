ff3a1f258cb9d38c3d6b90b0e8edc724a256c529
Finn-Delimata-May <D00276939@student.dkit.ie>
2025-01-03 16:03:05 +0000

    Calculate 95% Confidence Interval for Control
    The 95% Confidence Interval for the Control Group is calculated and
    displayed.
    This involves using the calculated Standard Error, and applying the
    T-Distribution (with 100 - 1 = 99 degrees of freedom) theory to
    calculate the Confidence Interval.

--- RScript1.R
+++ RScript1.R
@@ -2,13 +2,20 @@ library(readxl)
 participants_data_final_control <- read_excel("College Documents/Sem1_Data_Analy
 tics_for_Immersive_Environments/participants_data_final_control.xlsx")
 
-plot(participants_data_final_control$Change_in_Score)
-summary(participants_data_final_control$Change_in_Score)
+# The lines below establish variables from the data read in from the control 
+# group. "mc" means the mean from the control. 
+var.mc = mean(participants_data_final_control$Change_in_Score)
 var.s = sd(participants_data_final_control$Change_in_Score)
-var.rows = nrow(participants_data_final_control$Change_in_Score)
+var.rows = nrow(participants_data_final_control)
 
 library(readxl)
 participants_data_final_xp <- read_excel("College Documents/Sem1_Data_Analy
 tics_for_Immersive_Environments/participants_data_final_xp.xlsx")
 
-var.se = var.s/sqrt(var.rows)
+var.mx = mean(participants_data_final_xp$Change_in_Score)
+
+# The next few lines establish the 95% Confidence Interval for the Control
+var.se = var.s/sqrt(var.rows)   # Calculates Standard Error
+var.nse = qt(0.975, var.rows)   # The number of SEs away from the mean
+print(var.mc + var.nse*var.se)
+print(var.mc - var.nse*var.se)
