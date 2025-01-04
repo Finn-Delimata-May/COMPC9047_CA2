340274877fb272c1894de51e270a34b7b200c6cf
Finn-Delimata-May <D00276939@student.dkit.ie>
2025-01-04 21:18:06 +0000

    write out the report, and present.
    Writing out the report in R Notebook, and cleaning up the code in the R
    Script, as well as the workspace, to ensure full functionality. Much of
    the code was transferred over to the R Notebook for the final output.

--- RScript1.R
+++ RScript1.R
@@ -8,7 +8,8 @@ var.mc = mean(participants_data_final_control$Change_in_Score) # "mc" means the
                                                                # mean from the 
                                                                # control.
 var.s = sd(participants_data_final_control$Change_in_Score)
-var.df = nrow(participants_data_final_control) - 1
+var.rows = nrow(participants_data_final_control)
+var.df = var.rows - 1
 
 library(readxl)
 participants_data_final_xp <- read_excel("College Documents/Sem1_Data_Analy
@@ -18,7 +19,7 @@ var.mx = mean(participants_data_final_xp$Change_in_Score)
 
 # The next few lines establish the 95% Confidence Interval for the Control
 var.se = var.s/sqrt(var.rows)   # Calculates Standard Error
-var.nse = qt(0.975, var.rows)   # The number of SEs away from the mean
+var.nse = qt(0.975, var.df)   # The number of SEs away from the mean
 print(var.mc + var.nse*var.se)
 print(var.mc - var.nse*var.se)
 
@@ -30,3 +31,5 @@ boxplot(participants_data_final_control$Change_in_Score)
 boxplot(participants_data_final_xp$Change_in_Score)
 hist(participants_data_final_control$Change_in_Score)
 hist(participants_data_final_xp$Change_in_Score)
+
+
